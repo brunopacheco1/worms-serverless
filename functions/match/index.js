6ea78660ddd78d9ff99b683a5051988e00a2babb
Bruno da Silva <brunopacheco1@yahoo.com>
2019-11-27 21:34:42 +0100

    Adding new dependency and querying first for existing not running matchs

--- functions/match/index.js
+++ functions/match/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const admin = require("firebase-admin");
+const uuid = require("uuid/v1");
 const express = require("express");
 const { checkSchema, validationResult } = require("express-validator");
 const matchValidation = require("./match.validation");
@@ -19,6 +20,8 @@ app.use(cors(corsOption));
 app.use(express.json());
 
 app.post("/", checkSchema(matchValidation), (request, response) => {
+  const collection = admin.firestore().collection("match");
+
   const validation = validationResult(request);
 
   if (validation.errors.length > 0) {
@@ -27,27 +30,51 @@ app.post("/", checkSchema(matchValidation), (request, response) => {
 
   const newMatchConfig = request.body;
 
-  const match = {
+  const query = collection
+    .where("numberOfPlayers", "==", newMatchConfig.numberOfPlayers)
+    .where("status", "==", "WAITING_PLAYERS")
+    .limit(1);
+
+  const newMatch = {
     wall: "SOLID",
     opponentBody: "SOLID",
     difficulty: "HARD",
     playMode: "LONGEST_WORM",
-    numberOfPlayers: newMatchConfig.numberOfPlayers,
     mapSize: 30,
     status: "WAITING_PLAYERS",
-    players: [newMatch.playerId]
+    players: []
   };
 
-  return admin
-    .firestore()
-    .collection("match")
-    .add(match)
-    .then(doc =>
-      response.send({
-        ...match,
-        id: doc.id
-      })
-    );
+  return query
+    .get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        newMatch.id = uuid();
+        return newMatch;
+      } else {
+        let match = newMatch;
+        snapshot.forEach(doc => {
+          match = doc.data();
+        });
+        return match;
+      }
+    })
+    .then(match => {
+      match.numberOfPlayers = newMatchConfig.numberOfPlayers;
+
+      const newPlayers = new Set(match.players);
+      newPlayers.add(newMatchConfig.playerId);
+      match.players = Array.from(newPlayers);
+
+      if (match.players.length === match.numberOfPlayers) {
+        match.status = "RUNNING";
+      }
+
+      return collection
+        .doc(match.id)
+        .set(match)
+        .then(response.send(match));
+    });
 });
 
 module.exports = app;
