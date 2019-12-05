5dce5ac3b32e410050803b23c86266ec0547b4c2
Bruno da Silva <brunopacheco1@yahoo.com>
2019-12-05 20:10:30 +0100

    Removing projects info from git

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD1Up9RPpXr65OMntD1fUCk3dliLXFzTok",
-    authDomain: "worms-255512.firebaseapp.com",
-    databaseURL: "https://worms-255512.firebaseio.com",
-    projectId: "worms-255512",
-    storageBucket: "worms-255512.appspot.com",
-    messagingSenderId: "166457283272"
+    apiKey: "blablabla",
+    authDomain: "blablabla.firebaseapp.com",
+    databaseURL: "https://blablabla.firebaseio.com",
+    projectId: "blablabla",
+    storageBucket: "blablabla.appspot.com",
+    messagingSenderId: "blablabla"
   }
 };
