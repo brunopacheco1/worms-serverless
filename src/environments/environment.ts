fb342e22647d98c7761a3b02847d8a3cc92bba61
Bruno da Silva <brunopacheco1@yahoo.com>
2019-12-05 20:13:54 +0100

    Ignoring prod env config.

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "blablabla",
+    authDomain: "blablabla.firebaseapp.com",
+    databaseURL: "https://blablabla.firebaseio.com",
+    projectId: "blablabla",
+    storageBucket: "blablabla.appspot.com",
+    messagingSenderId: "blablabla"
+  }
+};
